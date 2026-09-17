2d413aebe9a21ba432440cabf818cf31d31fd6a0
matte_zhy <sallyace18@gmail.com>
2026-09-17 01:12:52 -0300

    Add files via upload

---
+++ Main (1).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int soma = 0;
+
+    for (int i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+            soma += i;
+        }
+    }
+
+    printf("A soma dos numeros pares de 1 a 100 é: %d\n", soma);
+    
+    return 0;
+}
